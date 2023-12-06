f028ce33b29acb90546bf1c8671e9ab807fef47d
Bruno Recht <brunorecht@outlook.de>
2023-12-06 14:31:21 +0100

    8

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ import telegram
 from dotenv import load_dotenv
 from pathlib import Path
 import openai
-from openai import OpenAI
 import sys
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
